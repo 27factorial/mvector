087eae9319a27e15a7aeec153c3d9944865cbea4
27factorial <stsppt28@gmail.com>
2017-08-31 00:16:43 -0700

    Add files via upload

--- mVector.hpp
+++ mVector.hpp
@@ -1,40 +1,74 @@
-// I've literally just started this library, don't judge me too hard.
-#include "stdafx.h"
+// STD::VECTOR AND MVECTOR ARE DIFFERENT THINGS!
+//mVector is used for mathematical vectors. (Think linear algebra / calculus.)
+//std::vector is (basically) an array that can be variable in size.
+
+#include "stdafx.h" // VS
+#include <cstdarg>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
-class mVector { // Vector is already taken by std::vector, so I used mVector for "math Vector".
-private: // Variables set by the constructor
-	double vectorX;
-	double vectorY;
+
+class mVector {
+private:
+
+	std::vector <double> vectorDimensions; // Using std::vector for making mathematical vectors?!?!? It allows us to make a vector of infnite dimensions. Why? Because I can.
+
 public:
-	mVector(double, double);
+	mVector(int, ...);
 
 	~mVector();
 
-	double giveX() {
-		return vectorX;
-	}
-
-	double giveY() {
-		return vectorY;
-	}
+	std::string giveDimensions();
 };
 
-mVector::mVector(double x = 0, double y = 0) {
-	vectorX = x;
-	vectorY = y;
+
+mVector::mVector(int num, ...) { // Using va_list with std::vector, we can add a vector of infnite dimensions.
+	std::va_list args;
+
+	va_start(args, num);
+	for (int x = 0; x < num; x++) {
+		vectorDimensions.insert(vectorDimensions.end(), va_arg(args, double)); // How axes work: x = index 0, y = index 1, z = index 2, etc.
+	}
+	va_end(args);
+
 }
 
+
 mVector::~mVector() {
+	// the compiler will make a destructor.
+}
+
+
+std::string mVector::giveDimensions() { //Messy code. I'll try to fix this up when I get a chance.
+	std::stringstream output;
+	for (int item = 0; item < vectorDimensions.size(); item++) {
+		if (item == vectorDimensions.size() - 1) {
+			output << vectorDimensions.at(item);
+		}
+		else {
+			output << vectorDimensions.at(item) << ", ";
+		}
+	}
+	return "[" + output.str() + "]";
+}
+
+
+int main() {
+
+	mVector vect(5, 1.0, 2.0, 3.0, 4.0, 5.0);
+	std::cout << vect.giveDimensions();
+
+	return 0;
 }
 
 /********************************************
-* TODO:										*
-* Make mVector scalable to 3 dimensions.    *
-* Add mathematical functions for vectors.   *
-* Add Matrices.                             *
-* Add math for Matrices.                    *
-* Cross compatibility.						*
-* Get rid of the todo list.                 *
+* TODO:
+/ Make mVector scalable to 3 dimensions. (Scales to infinite dimensions.)
+* Add mathematical functions for vectors.   
+* Add Matrices.
+* Add math for Matrices.
+* Cross compatibility.
+* Get rid of the todo list.
 *********************************************/
